3853f8b176538bcebfc2c3d2abac4290f0ebb344
Leonardo <144455265+Leonardo-Gianola@users.noreply.github.com>
2023-11-07 17:07:19 +0100

    Add files via upload

---
+++ WorldOfZuul/WorldOfZuul/Task1.cs
@@ -0,0 +1,29 @@
+using static WorldOfZuul.Program;
+namespace WorldOfZuul
+{
+    public class Task1
+    {
+        public void sandwich()
+        {
+            Console.WriteLine ("After searching throughout the university you find the physics professor,");
+            Console.WriteLine ("sitting in the library reading Brave New World by Aldous Huxley. The");
+            Console.WriteLine ("professor loves to read about the future, about what the world will be like");
+            Console.WriteLine ("in 50-100 years, on issues of sustainability, technology and sociology.");
+            Console.WriteLine (" ");
+            Console.WriteLine ("When talking to the professor he tells you that he will help you with everything and only");
+            Console.WriteLine ("asks you for one thing in exchange: to bring him a sandwich. So, you venture again");
+            Console.WriteLine ("to the university in search of a sandwich for the professor, you give it to him");
+            Console.WriteLine ("and here our adventure begins.");
+            Console.WriteLine (" ");
+            Console.WriteLine ("The professor, upon seeing your interest in sustainability, explained to you the importance");
+            Console.WriteLine ("of the Sustainability Development Goals.");
+            Console.WriteLine ("These are part of the 2030 Agenda, which outlines a collective vision for global");
+            Console.WriteLine ("peace and prosperity. At the heart of this agenda are the 17 Sustainable Development");
+            Console.WriteLine ("Goals (SDGs), a compelling call to action for all nations, regardless of their level");
+            Console.WriteLine ("of development. The SDGs highlight the interconnection between eradicating poverty"); 
+            Console.WriteLine ("and addressing issues such as health, education, inequality and economic growth."); 
+            Console.WriteLine ("At the same time, they urge efforts to combat climate change and protect our oceans");
+            Console.WriteLine ("and forests, underscoring the importance of united global collaboration.");
+        }
+    }
+}
