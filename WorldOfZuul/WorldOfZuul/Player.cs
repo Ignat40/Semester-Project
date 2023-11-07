d6022cc574a6b86df6c692aa27c55dbd37e0e78d
Ignat Bojinov <ignatbojinov@gmail.com>
2023-11-07 19:16:10 +0100

    chage her struct to Player.cs class

---
+++ WorldOfZuul/WorldOfZuul/Player.cs
@@ -0,0 +1,63 @@
+using System.Diagnostics;
+
+namespace WorldOfZuul
+{
+    public class Player
+    {
+        public string? PlayerName;
+        public List<string> Inventory = new List<string>();
+
+        public void DisplayPlayer()
+        {
+            Console.WriteLine("Enter your name Hero: ");
+            Console.Write(">");
+            string? inputName = Console.ReadLine();
+            if (inputName != null)
+            {
+                PlayerName = inputName;
+                Console.WriteLine($"You are our only hope {PlayerName}!");
+            }
+            else
+            {
+                PlayerName = "Mr. Eco";
+                Console.WriteLine($"You are our only hope {PlayerName}!");
+            }
+        }
+
+        public void DisplayInventory()
+        {
+            foreach(var el in Inventory)
+            {
+                Console.Write($" [{el}]");
+            }
+        }
+        public void AddToInventory(string item)
+        {
+            Inventory.Add(item);
+            Console.WriteLine($"Picked {item}");
+        }
+        public void RemoveFromIntentory()
+        {
+            Int32 inventoryCapacity = Inventory.Count;
+            if(inventoryCapacity <= 0)
+            {
+                Console.WriteLine("You don't have anything at the moment!");
+            }
+            else
+            {
+                System.Console.WriteLine("Enter index for the item you want to drop!");
+                for(int i = 0; i < Inventory.Count; i++)
+                {
+                    Console.WriteLine($"{i}. {Inventory[i]}");
+                }
+                Console.Write(">");
+                string? dropItem = Console.ReadLine();
+                Inventory.Remove(dropItem);
+            }
+        }
+
+        //Add score system for the player
+    }
+
+
+}
